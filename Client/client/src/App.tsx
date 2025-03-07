0766fffc50eabaea8c6b6508a895165fc4c6f5bf
Hetang Mehta <hetangmehta03@gmail.com>
2025-03-06 23:53:35 -0700

    added Routes to the application

--- Client/client/src/App.tsx
+++ Client/client/src/App.tsx
@@ -1,9 +1,10 @@
 // import { useState } from "react";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Layout from "@/pages/Layout";
 import Landing from "@/pages/Landing";
-// import Dashboard from "@/pages/Dashboard";
+import Dashboard from "@/pages/Dashboard";
 
 function App() {
   return (
@@ -12,7 +13,12 @@ function App() {
         Hello World
         <button onClick={() => setCount(count + 1)}>Count: {count}</button>
       </div> */}
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+
+      {/* <Landing /> */}
     </>
   );
 }
